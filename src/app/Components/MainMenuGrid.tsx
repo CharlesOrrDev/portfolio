9f1eed7fce85f9e1b08918cd431227b99b884301
HalfwitGoldfish <Chas2687@gmail.com>
2025-06-09 13:25:58 -0700

    cleaned up code a little bit so it was less lines

--- src/app/Components/MainMenuGrid.tsx
+++ src/app/Components/MainMenuGrid.tsx
@@ -2,26 +2,53 @@
 
 import React from 'react'
 
-const MainMenuGrid = () =>
+interface Iprops
+{
+  rowName: string
+}
+
+const MainMenuGrid = ({rowName}: Iprops) =>
 {
   return (
-    <div>
+    <>
 
       {/* Box One */}
-      <div className="col-start-1 row-start-1 grid">
-        <div className="bg-gray-950 bg-gradient-to-tl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-1">
+      <div
+        className={`
+          ${rowName == "One" ? "col-start-1 row-start-1 grid" : ""}
+          ${rowName == "Two" ? "col-start-1 row-start-2 grid" : ""}
+          ${rowName == "Three" ? "col-start-1 row-start-3 grid" : ""}
+        `}>
+        <div
+          className={`
+            ${rowName == "One" ? "bg-gray-950 bg-gradient-to-tl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-1" : ""}
+            ${rowName == "Two" || "Three" ? "bg-gray-950 bg-gradient-to-tl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-1 pt-[2px]" : ""}
+          `}
+        >
           <div className="bg-gray-950 w-full h-full"/>
         </div>
 
-        <div className="bg-gray-950 bg-gradient-to-tr from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-1 pr-[2px]">
+        <div
+          className={`
+            ${rowName == "One" ? "bg-gray-950 bg-gradient-to-tr from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-1 pr-[2px]" : ""}
+            ${rowName == "Two" || "Three" ? "bg-gray-950 bg-gradient-to-tr from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-1 pt-[2px] pr-[2px]" : ""}
+          `}>
           <div className="bg-gray-950 w-full h-full"/>
         </div>
 
-        <div className="bg-gray-950 bg-gradient-to-bl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-2 pb-[2px]">
+        <div
+          className={`
+            ${rowName == "One" || "Two" ? "bg-gray-950 bg-gradient-to-bl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-2 pb-[2px]" : ""}
+            ${rowName == "Three" ? "bg-gray-950 bg-gradient-to-bl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-2" : ""}
+          `}>
           <div className="bg-gray-950 w-full h-full"/>
         </div>
 
-        <div className="bg-gray-950 bg-gradient-to-br from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-2 pb-[2px] pr-[2px]">
+        <div
+          className={`
+            ${rowName == "One" || "Two" ? "bg-gray-950 bg-gradient-to-br from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-2 pb-[2px] pr-[2px]" : ""}
+            ${rowName == "Three" ? "bg-gray-950 bg-gradient-to-br from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-2 pr-[2px]" : ""}
+          `}>
           <div className="bg-gray-950 w-full h-full"/>
         </div>
       </div>
@@ -29,20 +56,41 @@ const MainMenuGrid = () =>
 
 
       {/* Box Two */}
-      <div className="col-start-2 row-start-1 grid">
-        <div className="bg-gray-950 bg-gradient-to-tl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-1 pl-[2px]">
+      <div
+        className={`
+          ${rowName == "One" ? "col-start-2 row-start-1 grid" : ""}
+          ${rowName == "Two" ? "col-start-2 row-start-2 grid" : ""}
+          ${rowName == "Three" ? "col-start-2 row-start-3 grid" : ""}
+        `}>
+        <div
+          className={`
+            ${rowName == "One" ? "bg-gray-950 bg-gradient-to-tl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-1 pl-[2px]" : ""}
+            ${rowName == "Two" || "Three" ? "bg-gray-950 bg-gradient-to-tl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-1 pt-[2px] pl-[2px]" : ""}
+          `}>
           <div className="bg-gray-950 w-full h-full"/>
         </div>
 
-        <div className="bg-gray-950 bg-gradient-to-tr from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-1 pr-[2px]">
+        <div
+          className={`
+            ${rowName == "One" ? "bg-gray-950 bg-gradient-to-tr from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-1 pr-[2px]" : ""}
+            ${rowName == "Two" || "Three" ? "bg-gray-950 bg-gradient-to-tr from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-1 pt-[2px] pr-[2px]" : ""}
+          `}>
           <div className="bg-gray-950 w-full h-full"/>
         </div>
 
-        <div className="bg-gray-950 bg-gradient-to-bl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-2 pb-[2px] pl-[2px]">
+        <div
+          className={`
+            ${rowName == "One" || "Two" ? "bg-gray-950 bg-gradient-to-bl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-2 pb-[2px] pl-[2px]" : ""}
+            ${rowName == "Three" ? "bg-gray-950 bg-gradient-to-bl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-2 pl-[2px]" : ""}
+          `}>
           <div className="bg-gray-950 w-full h-full"/>
         </div>
 
-        <div className="bg-gray-950 bg-gradient-to-br from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-2 pb-[2px] pr-[2px]">
+        <div
+          className={`
+            ${rowName == "One" || "Two" ? "bg-gray-950 bg-gradient-to-br from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-2 pb-[2px] pr-[2px]" : ""}
+            ${rowName == "Three" ? "bg-gray-950 bg-gradient-to-br from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-2 pr-[2px]" : ""}
+          `}>
           <div className="bg-gray-950 w-full h-full"/>
         </div>
       </div>
@@ -50,20 +98,41 @@ const MainMenuGrid = () =>
 
 
       {/* Box Three */}
-      <div className="col-start-3 row-start-1 grid">
-        <div className="bg-gray-950 bg-gradient-to-tl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-1 pl-[2px]">
+      <div
+        className={`
+          ${rowName == "One" ? "col-start-3 row-start-1 grid" : ""}
+          ${rowName == "Two" ? "col-start-3 row-start-2 grid" : ""}
+          ${rowName == "Three" ? "col-start-3 row-start-3 grid" : ""}
+        `}>
+        <div
+          className={`
+            ${rowName == "One" ? "bg-gray-950 bg-gradient-to-tl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-1 pl-[2px]" : ""}
+            ${rowName == "Two" || "Three" ? "bg-gray-950 bg-gradient-to-tl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-1 pt-[2px] pl-[2px]" : ""}
+          `}>
           <div className="bg-gray-950 w-full h-full"/>
         </div>
 
-        <div className="bg-gray-950 bg-gradient-to-tr from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-1 pr-[2px]">
+        <div
+          className={`
+            ${rowName == "One" ? "bg-gray-950 bg-gradient-to-tr from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-1 pr-[2px]" : ""}
+            ${rowName == "Two" || "Three" ? "bg-gray-950 bg-gradient-to-tr from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-1 pt-[2px] pr-[2px]" : ""}
+          `}>
           <div className="bg-gray-950 w-full h-full"/>
         </div>
 
-        <div className="bg-gray-950 bg-gradient-to-bl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-2 pb-[2px] pl-[2px]">
+        <div
+          className={`
+            ${rowName == "One" || "Two" ? "bg-gray-950 bg-gradient-to-bl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-2 pb-[2px] pl-[2px]" : ""}
+            ${rowName == "Three" ? "bg-gray-950 bg-gradient-to-bl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-2 pl-[2px]" : ""}
+          `}>
           <div className="bg-gray-950 w-full h-full"/>
         </div>
 
-        <div className="bg-gray-950 bg-gradient-to-br from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-2 pb-[2px] pr-[2px]">
+        <div
+          className={`
+            ${rowName == "One" || "Two" ? "bg-gray-950 bg-gradient-to-br from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-2 pb-[2px] pr-[2px]" : ""}
+            ${rowName == "Three" ? "bg-gray-950 bg-gradient-to-br from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-2 pr-[2px]" : ""}
+          `}>
           <div className="bg-gray-950 w-full h-full"/>
         </div>
       </div>
@@ -71,20 +140,41 @@ const MainMenuGrid = () =>
       
 
       {/* Box Four */}
-      <div className="col-start-4 row-start-1 grid">
-        <div className="bg-gray-950 bg-gradient-to-tl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-1 pl-[2px]">
+      <div
+        className={`
+          ${rowName == "One" ? "col-start-4 row-start-1 grid" : ""}
+          ${rowName == "Two" ? "col-start-4 row-start-2 grid" : ""}
+          ${rowName == "Three" ? "col-start-4 row-start-3 grid" : ""}
+        `}>
+        <div
+          className={`
+            ${rowName == "One" ? "bg-gray-950 bg-gradient-to-tl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-1 pl-[2px]" : ""}
+            ${rowName == "Two" || "Three" ? "bg-gray-950 bg-gradient-to-tl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-1 pt-[2px] pl-[2px]" : ""}
+          `}>
           <div className="bg-gray-950 w-full h-full"/>
         </div>
 
-        <div className="bg-gray-950 bg-gradient-to-tr from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-1 pr-[2px]">
+        <div
+          className={`
+            ${rowName == "One" ? "bg-gray-950 bg-gradient-to-tr from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-1 pr-[2px]" : ""}
+            ${rowName == "Two" || "Three" ? "bg-gray-950 bg-gradient-to-tr from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-1 pt-[2px] pr-[2px]" : ""}
+          `}>
           <div className="bg-gray-950 w-full h-full"/>
         </div>
 
-        <div className="bg-gray-950 bg-gradient-to-bl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-2 pb-[2px] pl-[2px]">
+        <div
+          className={`
+            ${rowName == "One" || "Two" ? "bg-gray-950 bg-gradient-to-bl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-2 pb-[2px] pl-[2px]" : ""}
+            ${rowName == "Three" ? "bg-gray-950 bg-gradient-to-bl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-2 pl-[2px]" : ""}
+          `}>
           <div className="bg-gray-950 w-full h-full"/>
         </div>
 
-        <div className="bg-gray-950 bg-gradient-to-br from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-2 pb-[2px] pr-[2px]">
+        <div
+          className={`
+            ${rowName == "One" || "Two" ? "bg-gray-950 bg-gradient-to-br from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-2 pb-[2px] pr-[2px]" : ""}
+            ${rowName == "Three" ? "bg-gray-950 bg-gradient-to-br from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-2 pr-[2px]" : ""}
+          `}>
           <div className="bg-gray-950 w-full h-full"/>
         </div>
       </div>
@@ -92,26 +182,47 @@ const MainMenuGrid = () =>
 
 
       {/* Box Five */}
-      <div className="col-start-5 row-start-1 grid">
-        <div className="bg-gray-950 bg-gradient-to-tl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-1 pl-[2px]">
+      <div
+        className={`
+          ${rowName == "One" ? "col-start-5 row-start-1 grid" : ""}
+          ${rowName == "Two" ? "col-start-5 row-start-2 grid" : ""}
+          ${rowName == "Three" ? "col-start-5 row-start-3 grid" : ""}
+        `}>
+        <div
+          className={`
+            ${rowName == "One" ? "bg-gray-950 bg-gradient-to-tl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-1 pl-[2px]" : ""}
+            ${rowName == "Two" || "Three" ? "bg-gray-950 bg-gradient-to-tl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-1 pt-[2px] pl-[2px]" : ""}
+          `}>
           <div className="bg-gray-950 w-full h-full"/>
         </div>
 
-        <div className="bg-gray-950 bg-gradient-to-tr from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-1">
+        <div
+          className={`
+            ${rowName == "One" ? "bg-gray-950 bg-gradient-to-tr from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-1" : ""}
+            ${rowName == "Two" || "Three" ? "bg-gray-950 bg-gradient-to-tr from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-1 pt-[2px]" : ""}
+          `}>
           <div className="bg-gray-950 w-full h-full"/>
         </div>
 
-        <div className="bg-gray-950 bg-gradient-to-bl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-2 pb-[2px] pl-[2px]">
+        <div
+          className={`
+            ${rowName == "One" || "Two" ? "bg-gray-950 bg-gradient-to-bl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-2 pb-[2px] pl-[2px]" : ""}
+            ${rowName == "Three" ? "bg-gray-950 bg-gradient-to-bl from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-1 row-start-2 pl-[2px]" : ""}
+          `}>
           <div className="bg-gray-950 w-full h-full"/>
         </div>
 
-        <div className="bg-gray-950 bg-gradient-to-br from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-2 pb-[2px]">
+        <div
+          className={`
+            ${rowName == "One" || "Two" ? "bg-gray-950 bg-gradient-to-br from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-2 pb-[2px]" : ""}
+            ${rowName == "Three" ? "bg-gray-950 bg-gradient-to-br from-gray-700/40 via-gray-400/40 to-gray-700/40 col-start-2 row-start-2" : ""}
+          `}>
           <div className="bg-gray-950 w-full h-full"/>
         </div>
       </div>
       {/* Box Five */}
 
-    </div>
+    </>
   )
 }
 
